e174342e9b229365f1a8c7b2f5b4995497702987
kwonyo9 <46385166+kwonyo9@users.noreply.github.com>
2019-07-19 17:04:51 -0700

    Update MainView.java

--- MainView.java
+++ MainView.java
@@ -1,8 +1,5 @@
 package Proj;
 
-
-import java.awt.BasicStroke;
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -38,8 +35,8 @@ public class MainView {
 	private final JLabel monthLabel = new JLabel();
 	private final JPanel monthPanel;
 	private final JPanel eventsPanel;
-	private final JLabel eventsHeader = new JLabel();
 	private final JTextArea tf = new JTextArea();
+	
 	public static enum DAYS_OF_WEEK {
 		Sun, Mon, Tue, Wed, Thu, Fri, Sat
 	};
@@ -49,18 +46,15 @@ public class MainView {
 		this.model = model;
 		this.cal = model.getCal();
 		MainButton c = new MainButton(model);
-		c.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); 
+		c.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3)); 
 		JFrame frame = new JFrame();
 		JPanel month = new JPanel();
-		JButton previousMonth = new JButton("<<");
-		JButton nextMonth = new JButton(">>");
+		JButton previousMonth = new JButton("<");
+		JButton nextMonth = new JButton(">");
 		
-		previousMonth.setOpaque(true);
-		previousMonth.setBorderPainted(false); // makes color bg show properly on Mac
-		previousMonth.setFocusable(false);
-		previousMonth.setBackground(new Color(238,238,238));
+		previousMonth.setBackground(new Color(189,189,189));
 		previousMonth.setForeground(Color.BLACK);
-		previousMonth.setFont(new Font("Tahoma", Font.BOLD, 16));
+		
 
 		previousMonth.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -69,13 +63,9 @@ public class MainView {
 			}
 		});
 
-		nextMonth.setOpaque(true);
-		nextMonth.setBorderPainted(false);
-		nextMonth.setFocusable(false);
 		nextMonth.setBackground(new Color(238,238,238));
 		nextMonth.setForeground(Color.BLACK);
-		nextMonth.setFont(new Font("Tahoma", Font.BOLD, 16));
-
+		nextMonth.setSize(new Dimension(30,30));
 		nextMonth.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
@@ -93,8 +83,8 @@ public class MainView {
 		month.add(nextMonth, BorderLayout.WEST);
 		monthWrap.add(month, BorderLayout.NORTH);
 		monthWrap.add(monthPanel, BorderLayout.CENTER);
-		monthWrap.setSize(new Dimension(400,400));
-		monthWrap.setBorder(BorderFactory.createEmptyBorder(40, 40, 40, 40)); 
+		monthWrap.setSize(new Dimension(300,300));
+		monthWrap.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5)); 
 		drawMonth(monthPanel);
 		
 		scrollPane = new JScrollPane();
@@ -102,11 +92,11 @@ public class MainView {
 		eventsPanel.add(tf);
 		//dayPanel.setLayout(new BoxLayout(dayPanel, BoxLayout.PAGE_AXIS));
 		eventsPanel.setLayout(new BorderLayout()); 
-		scrollPane.getVerticalScrollBar().setUnitIncrement(16);
+		scrollPane.getVerticalScrollBar().setUnitIncrement(8);
 		scrollPane.getViewport().add(eventsPanel);
-		scrollPane.setPreferredSize(new Dimension(700, 300));
+		scrollPane.setPreferredSize(new Dimension(500, 200));
 
-		frame.add(c, BorderLayout.NORTH);
+		eventsPanel.add(c,BorderLayout.BEFORE_FIRST_LINE);
 		frame.add(monthWrap, BorderLayout.WEST);
 		frame.add(scrollPane, BorderLayout.EAST);
 		frame.setResizable(false);
@@ -128,13 +118,13 @@ public class MainView {
 	private void drawMonth(JPanel monthPanel) {
 		//Gets the month and the year and sets it on the top of the month view
 		monthLabel.setText(new SimpleDateFormat("MMMM yyyy").format(cal.getTime()));
-		monthLabel.setFont(new Font("Tahoma", Font.BOLD, 24));
+		monthLabel.setFont(new Font("Arial", Font.BOLD, 12));
 
 		//Add Week Labels at top of Month View
 		for (int i = 0; i<7; i++) {
 			JLabel day = new JLabel("" + DAYS_OF_WEEK.values()[i], SwingConstants.CENTER);
-			day.setBorder(new CompoundBorder(day.getBorder(), new EmptyBorder(10, 10, 10, 10)));
-			day.setFont(new Font("Tahoma", Font.BOLD, 12));
+			day.setBorder(new CompoundBorder(day.getBorder(), new EmptyBorder(2, 2, 2, 2)));
+			day.setFont(new Font("Arial", Font.BOLD, 10));
 			if(i == 0 || i == 6) {
 				day.setForeground(Color.RED);
 			}
@@ -163,9 +153,9 @@ public class MainView {
 					public void mouseExited(MouseEvent e) {}
 				});
 				if (dayNumber == cal.get(Calendar.DAY_OF_MONTH)) {
-					day.setBorder(BorderFactory.createLineBorder(Color.black));
+					day.setBorder(BorderFactory.createLineBorder(Color.WHITE));
 				}
-				day.setFont(new Font("Tahoma", Font.BOLD, 16));
+				day.setFont(new Font("Arial", Font.BOLD, 16));
 				//Adds the JLabel to the JPanel
 				monthPanel.add(day);
 			}
