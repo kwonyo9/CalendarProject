1bb9ba5a914b98d5f2caf012293055f61ea32960
Aldo Gutierrez <aldodario1@hotmail.es>
2019-08-02 16:09:20 -0700

    Changed name of Driver.java

---
+++ proj/Main.java
@@ -0,0 +1,14 @@
+package proj;
+
+public class Main
+{
+	/**
+	 * Main function
+	 * to start the program.
+	 */
+	public static void main(String[] args)
+	{
+		CalendarView tester = new CalendarView();
+		tester.setVisible(true);
+	}
+}
